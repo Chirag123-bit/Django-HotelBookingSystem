6f5ce2c717e0993700a674291ba383db736ac0e8
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-04-03 21:16:04 +0545

    update profile

--- users/views.py
+++ users/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from . forms import RegisterForm
+from . forms import ProfileUpdateForm, UserUpdateForm
 
 # Create your views here.
 
-
-def login(request):
-    return HttpResponse("Login")
 
 def register(request):
     if request.method == "POST":
@@ -21,3 +19,23 @@ def register(request):
         "form":form
     }
     return render(request, "users/register.html",context)
+
+
+def profile(request):
+    if(request.method=="POST"):
+        u_form = UserUpdateForm(request.POST or None, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST or None, request.FILES or None, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect("profile-page")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context={
+        "u_form":u_form,
+        "p_form":p_form
+    }
+    return render(request, "users/profile.html",context)
